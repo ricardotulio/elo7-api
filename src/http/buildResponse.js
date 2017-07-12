ee2c055818836d606ccdadc756ff1261d03a0074
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-07-12 00:50:16 -0300

    fix: fix unresolved conflict that was commited

--- src/http/buildResponse.js
+++ src/http/buildResponse.js
@@ -1,6 +1,5 @@
 import { curry } from 'ramda'
 
-<<<<<<< HEAD
 const buildResponse = curry((endpoint, req, res) => {
   const success = content => buildSuccessResponse(content, res)
   const error = () => buildErrorResponse(res)
@@ -8,24 +7,10 @@ const buildResponse = curry((endpoint, req, res) => {
   return endpoint().then(success)
     .catch(error)
 })
-=======
 const buildErrorResponse = (err, res) =>
   res.status(500).send('Something broke')
->>>>>>> 13d6776... improve testes
 
 const buildSuccessResponse = (content, res) =>
   res.status(200).send(content)
-
-<<<<<<< HEAD
-const buildErrorResponse = (res) => {
-  return res.status(500)
-    .send('Something broke')
-}
-=======
-const buildResponse = curry((endpoint, req, res) =>
-  endpoint(req, res)
-    .then(content => buildSuccessResponse(content, res))
-    .catch(error => buildErrorResponse(error, response)))
->>>>>>> 13d6776... improve testes
 
 export default buildResponse
